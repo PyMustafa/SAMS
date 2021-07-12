7938ea4f290e133ec1828e53c057e5e8440eba28
ahmedAbdlsalam <ahmedmoabdelsalam98@gmail.com>
2021-07-12 23:25:06 +0200

    Update encodingModel.py
    change signal to the flag table

--- SAMS/encodingModel.py
+++ SAMS/encodingModel.py
@@ -12,7 +12,7 @@ from django.dispatch import receiver
 
 # pip install opencv-python
 
-@receiver(post_save, sender=Image)
+@receiver(post_save, sender=Flag)
 def job(sender, **kwargs):
     print("the job started ..............")
     images = []
